d92906e1aee1b18c1bc72cf7c8724436cc72ea25
AlexsJones <alexsimonjones@gmail.com>
2012-04-12 10:27:08 +0200

    send broadcast method..

--- src/socketcontrol.cpp
+++ src/socketcontrol.cpp
@@ -185,5 +185,48 @@ void socketControl::subscribeBroadcast(string port)
 	close(sockfd);
   	 
 }
+void socketcontrol::sendBroadcast(string host, string port, string message)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(host.c_str(),port.c_str(), &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return;
+    }
+
+    // loop through all the results and make a socket
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to bind socket\n");
+        return;
+    }
+
+    if ((numbytes = sendto(sockfd, message.c_str(), strlen(message.c_str()), 0,
+                           p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, host.c_str());
+    close(sockfd);
+}
 
 
